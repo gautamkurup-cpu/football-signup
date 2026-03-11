46f9b8078bf4501488e5a3d8df553a6ad2aad1e7
gautamkurup-cpu <gautam.kurup@gmail.com>
2026-03-11 16:55:18 +0000

    Update state.js

--- netlify/functions/state.js
+++ netlify/functions/state.js
@@ -1,8 +1,26 @@
 import { getState } from "./_store.js";
 
 export default async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(await getState())
-  };
+  try {
+    const state = await getState();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(state)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        error: "Function crashed",
+        message: err.message || String(err)
+      })
+    };
+  }
 };
