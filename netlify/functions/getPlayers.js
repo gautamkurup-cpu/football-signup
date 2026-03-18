98f0c987bcdd52d6f96441cb7f13803f8bfb875d
gautamkurup-cpu <gautam.kurup@gmail.com>
2026-03-18 20:43:17 +0000

    Update getPlayers.js

--- netlify/functions/getPlayers.js
+++ netlify/functions/getPlayers.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async () => {
-  const file = path.join(__dirname, "../../data/players.json");
+  const file = path.join(process.cwd(), "data/players.json");
   const data = fs.readFileSync(file, "utf8");
+
   return {
     statusCode: 200,
     body: data
