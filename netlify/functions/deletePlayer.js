2e2b04c6e63710a812639c7a02f1637784ec772e
gautamkurup-cpu <gautam.kurup@gmail.com>
2026-03-18 20:19:54 +0000

    Create deletePlayer.js

---
+++ netlify/functions/deletePlayer.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event) => {
+  const file = path.join(__dirname, "../../data/players.json");
+  const players = JSON.parse(fs.readFileSync(file, "utf8"));
+
+  const id = event.queryStringParameters.id;
+
+  const filtered = players.filter(p => p.id !== id);
+
+  fs.writeFileSync(file, JSON.stringify(filtered, null, 2));
+
+  return { statusCode: 200, body: "OK" };
+};
