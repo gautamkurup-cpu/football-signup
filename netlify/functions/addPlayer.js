cd7b4114ce924848a167cff9739fb7a3103ef812
gautamkurup-cpu <gautam.kurup@gmail.com>
2026-03-18 20:18:30 +0000

    Create addPlayer.js

---
+++ netlify/functions/addPlayer.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
+exports.handler = async (event) => {
+  const file = path.join(__dirname, "../../data/players.json");
+  const players = JSON.parse(fs.readFileSync(file, "utf8"));
+
+  const newPlayer = JSON.parse(event.body);
+  newPlayer.id = uuid();
+
+  players.push(newPlayer);
+
+  fs.writeFileSync(file, JSON.stringify(players, null, 2));
+
+  return { statusCode: 200, body: "OK" };
+};
