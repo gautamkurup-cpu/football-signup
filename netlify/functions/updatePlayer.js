437e0e00d19a862b81bc1a430f6e852978970919
gautamkurup-cpu <gautam.kurup@gmail.com>
2026-03-18 20:19:11 +0000

    Create updatePlayer.js

---
+++ netlify/functions/updatePlayer.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.handler = async (event) => {
+  const file = path.join(__dirname, "../../data/players.json");
+  const players = JSON.parse(fs.readFileSync(file, "utf8"));
+
+  const updated = JSON.parse(event.body);
+
+  const index = players.findIndex(p => p.id === updated.id);
+  players[index] = updated;
+
+  fs.writeFileSync(file, JSON.stringify(players, null, 2));
+
+  return { statusCode: 200, body: "OK" };
+};
