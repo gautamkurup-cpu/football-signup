534adc091687e887ff2bea01efbf34f60dd99af1
gautamkurup-cpu <gautam.kurup@gmail.com>
2026-03-20 09:01:40 +0000

    Update playerdb.js

--- playerdb.js
+++ playerdb.js
@@ -2,53 +2,36 @@ async function loadPlayers() {
   const res = await fetch("/.netlify/functions/getPlayers");
   const players = await res.json();
 
-  const body = document.getElementById("playerBody");
-  body.innerHTML = "";
+  const tbody = document.getElementById("playerTableBody");
+  tbody.innerHTML = "";
 
-  players.forEach(p => {
-    const row = document.createElement("tr");
+  players.forEach(player => {
+    const tr = document.createElement("tr");
 
-    row.innerHTML = `
-      <td>${p.name}</td>
-      <td>${p.ballControl}</td>
-      <td>${p.shooting}</td>
-      <td>${p.passing}</td>
-      <td>${p.defending}</td>
-      <td>${p.physicality}</td>
-      <td>${p.workRate}</td>
-      <td>${p.goalkeeping}</td>
-      <td><button onclick="editPlayer('${p.id}')">Edit</button></td>
-      <td><button onclick="deletePlayer('${p.id}')">Delete</button></td>
+    const a = player.attributes;
+    const c = player.computed;
+
+    tr.innerHTML = `
+      <td>${player.name}</td>
+      <td>${a.ballControl}</td>
+      <td>${a.pace}</td>
+      <td>${a.shooting}</td>
+      <td>${a.passing}</td>
+      <td>${a.defending}</td>
+      <td>${a.workRate}</td>
+      <td>${a.goalKeeping}</td>
+      <td>${c.bestPosition}</td>
+      <td>${c.overallRating}</td>
+      <td><button class="edit-btn" onclick="editPlayer('${player.id}')">Edit</button></td>
     `;
 
-    body.appendChild(row);
+    tbody.appendChild(tr);
   });
 }
 
-document.getElementById("addBtn").onclick = async () => {
-  const player = {
-    name: document.getElementById("name").value,
-    ballControl: Number(document.getElementById("ballControl").value),
-    shooting: Number(document.getElementById("shooting").value),
-    passing: Number(document.getElementById("passing").value),
-    defending: Number(document.getElementById("defending").value),
-    physicality: Number(document.getElementById("physicality").value),
-    workRate: Number(document.getElementById("workRate").value),
-    goalkeeping: Number(document.getElementById("goalkeeping").value)
-  };
-
-  await fetch("/.netlify/functions/addPlayer", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(player)
-  });
-
-  loadPlayers();
-};
-
-async function deletePlayer(id) {
-  await fetch("/.netlify/functions/deletePlayer?id=" + id);
-  loadPlayers();
+function editPlayer(id) {
+  // We will build editplayer.html next
+  window.location.href = `editplayer.html?id=${id}`;
 }
 
 loadPlayers();
